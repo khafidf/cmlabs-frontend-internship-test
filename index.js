cb4e83c1489e287e26e6b0fab0b27b35c5c5ad15
khafidf <khfd027@gmail.com>
2023-06-12 12:33:32 +0700

    setup

---
+++ index.js
@@ -0,0 +1,7 @@
+// const meal = async () => {
+// 	const response = await fetch(
+// 		"https://www.themealdb.com/api/json/v1/1/categories.php"
+// 	);
+// 	const datas = await response.json();
+// 	console.log(datas);
+// };
